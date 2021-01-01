e2423831edaf5d3372d4d6ba37a7c9881895bdf1
Omobolaji123 <wisdomomobolaji@gmail.com>
2021-01-01 21:11:46 +0100

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '0c680890-217a-44ae-9d52-f75255cbfbc4';
+export const MODAL_CLOSE_SEC = 2.5;
